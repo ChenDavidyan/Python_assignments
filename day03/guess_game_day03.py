9449148f569d4af8b817764f8f0da201df197338
ChenDavidyan <chendavidyan@gmail.com>
2024-05-06 14:02:05 +0300

    added the is spacial char function

--- day03/guess_game_day03.py
+++ day03/guess_game_day03.py
@@ -12,22 +12,29 @@ def offer_new_game():
         main()
 
 
+def is_spacial_char(guess, answer, cnt):
+    match guess.lower():
+        case "x":
+            return guess, answer, cnt, "break_loop"
+        case "n":
+            offer_new_game()
+            return guess, answer, cnt, "break_loop"
+        case "s":
+            cnt -= 1
+            print(f"The right answer is {answer}")
+            guess, cnt = get_new_guess(cnt)
+            return guess, answer, cnt, "continue"
+    return guess, answer, cnt, "continue"
+
+
 def main():
     answer = random.randrange(1, 21)
     guess, cnt = get_new_guess(0)
 
     while True:
-        match guess.lower():
-            case "x":
-                break
-            case "n":
-                offer_new_game()
-                break
-            case "s":
-                cnt -= 1
-                print(f"The right answer is {answer}")
-                guess, cnt = get_new_guess(cnt)
-
+        guess, answer, cnt, desicion = is_spacial_char(guess, answer, cnt)
+        if desicion == "break_loop":
+            break
         try:
             if int(guess) == answer:
                 print("You are right!")
