10303c127f0f7f8a6a8247ed0d963ea8322ef1b2
ChenDavidyan <chendavidyan@gmail.com>
2024-05-06 14:42:30 +0300

    added the invalid_input function

--- day03/guess_game_day03.py
+++ day03/guess_game_day03.py
@@ -26,6 +26,24 @@ def is_spacial_char(guess, answer, cnt):
             return guess, answer, cnt, "continue"
     return guess, answer, cnt, "continue"
 
+def compare_guess_to_answer(guess, answer, cnt):
+            if int(guess) == answer:
+                print("You are right!")
+                print(f"Number of guesses: {cnt} ")
+                offer_new_game()
+                return guess, answer, cnt, "break_loop"
+            if int(guess) > answer:
+                print("Try again! Hint: your guess was too big")
+            if int(guess) < answer:
+                print("Try again! Hint: your guess was too small")
+            guess, cnt = get_new_guess(cnt)
+            return guess, answer, cnt, "continue"
+
+def invalid_input (cnt):
+    cnt -= 1
+    print("Invalid input. Try again")
+    return cnt
+
 
 def main():
     answer = random.randrange(1, 21)
@@ -36,19 +54,11 @@ def main():
         if desicion == "break_loop":
             break
         try:
-            if int(guess) == answer:
-                print("You are right!")
-                print(f"Number of guesses: {cnt} ")
-                offer_new_game()
-                break
-            if int(guess) > answer:
-                print("Try again! Hint: your guess was too big")
-            if int(guess) < answer:
-                print("Try again! Hint: your guess was too small")
-            guess, cnt = get_new_guess(cnt)
+            guess, answer, cnt, desicion = compare_guess_to_answer(guess, answer, cnt)
+            if desicion == "break_loop":
+                break   
         except ValueError:
-            cnt -= 1
-            print("Invalid input. Try again")
+            cnt = invalid_input(cnt)
             guess, cnt = get_new_guess(cnt)
 
 
