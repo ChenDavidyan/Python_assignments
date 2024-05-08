7ef021a92bbfd0dacd61a9bac570de7d73cffacd
ChenDavidyan <chendavidyan@gmail.com>
2024-05-08 15:52:16 +0300

    day03

--- day03/guess_game_day03.py
+++ day03/guess_game_day03.py
@@ -1,65 +1,68 @@
 import random
 
 
-def get_new_guess(cnt):
-    guess = input("please write your guess")
-    cnt += 1
-    return guess, cnt
-
-
 def offer_new_game():
-    if input("would you like to play again? (y/n)").lower() == "y":
-        main()
+    choice = input("would you like to play again? (y/n)").lower()
+    while True:
+        if choice == "y":
+            return "new_game"
+        elif choice == "n":
+            return "exit_game"
+        else:
+            choice = input("Invalid input! would you like to play again? (y/n)")
 
 
-def is_spacial_char(guess, answer, cnt):
+def is_spacial_char(guess, answer):
     match guess.lower():
         case "x":
-            return guess, answer, cnt, "break_loop"
+            return "exit_game"
         case "n":
-            offer_new_game()
-            return guess, answer, cnt, "break_loop"
+            desicion = offer_new_game()
+            return desicion
         case "s":
-            cnt -= 1
             print(f"The right answer is {answer}")
-            guess, cnt = get_new_guess(cnt)
-            return guess, answer, cnt, "continue"
-    return guess, answer, cnt, "continue"
+            return "new_guess"
+    return "continue"
+
 
 def compare_guess_to_answer(guess, answer, cnt):
-            if int(guess) == answer:
-                print("You are right!")
-                print(f"Number of guesses: {cnt} ")
-                offer_new_game()
-                return guess, answer, cnt, "break_loop"
-            if int(guess) > answer:
-                print("Try again! Hint: your guess was too big")
-            if int(guess) < answer:
-                print("Try again! Hint: your guess was too small")
-            guess, cnt = get_new_guess(cnt)
-            return guess, answer, cnt, "continue"
-
-def invalid_input (cnt):
-    cnt -= 1
-    print("Invalid input. Try again")
-    return cnt
+    if int(guess) == answer:
+        print("You are right!")
+        print(f"Number of guesses: {cnt} ")
+        decision = offer_new_game()
+        return decision
+    elif int(guess) > answer:
+        print("Try again! Hint: your guess was too big")
+        return "new_guess"
+    elif int(guess) < answer:
+        print("Try again! Hint: your guess was too small")
+        return "new_guess"
 
 
 def main():
-    answer = random.randrange(1, 21)
-    guess, cnt = get_new_guess(0)
-
     while True:
-        guess, answer, cnt, desicion = is_spacial_char(guess, answer, cnt)
-        if desicion == "break_loop":
+        answer = random.randrange(1, 21)
+        cnt = 1
+
+        while True:
+            guess = input("please write your guess")
+            desicion = is_spacial_char(guess, answer)
+            if desicion == "new_game" or desicion == "exit_game":
+                break
+            elif desicion == "new_guess":
+                continue
+
+            try:
+                desicion = compare_guess_to_answer(guess, answer, cnt)
+                if desicion == "new_game" or desicion == "exit_game":
+                    break
+            except ValueError:
+                print("Invalid input!")
+                continue
+            cnt += 1
+
+        if desicion == "exit_game":
             break
-        try:
-            guess, answer, cnt, desicion = compare_guess_to_answer(guess, answer, cnt)
-            if desicion == "break_loop":
-                break   
-        except ValueError:
-            cnt = invalid_input(cnt)
-            guess, cnt = get_new_guess(cnt)
 
 
 main()
